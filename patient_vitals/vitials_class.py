a02f611f2ddb633abbc8f3d73c16c3715da109db
chromeMatic <armanibrwn@gmail.com>
2026-05-10 15:32:40 -0500

    More changes to notes endpoint

--- patient_vitals/vitials_class.py
+++ patient_vitals/vitials_class.py
@@ -2,23 +2,23 @@ from pydantic import BaseModel
 
 class patient_vitals_insert(BaseModel):
     patient_id:str
-    blood_pressure_top_value:str
-    blood_pressure_bottom_value:str
-    body_temperature:str
-    pulse_oximetry:str
-    plus_rate:str
-    respiratory_rate:str
-    height:str
-    body_weight:str
+    blood_pressure_top_value:int
+    blood_pressure_bottom_value:int
+    body_temperature:int
+    pulse_oximetry:int
+    plus_rate:int
+    respiratory_rate:int
+    height:int
+    body_weight:int
 
 class patient_vitals_edit(BaseModel):
     patient_vitals_id:str
     patient_id:str
-    blood_pressure_top_value:str
-    blood_pressure_bottom_value:str
-    body_temperature:str
-    pulse_oximetry:str
-    plus_rate:str
-    respiratory_rate:str
-    height:str
-    body_weight:str
+    blood_pressure_top_value:int
+    blood_pressure_bottom_value:int
+    body_temperature:int
+    pulse_oximetry:int
+    plus_rate:int
+    respiratory_rate:int
+    height:int
+    body_weight:int

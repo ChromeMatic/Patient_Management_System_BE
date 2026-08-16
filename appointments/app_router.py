bb807af3fa6d310082a1d2b845923fe4a62ca36b
chromeMatic <armanibrwn@gmail.com>
2026-08-16 18:21:29 -0500

    more changes addded

--- appointments/app_router.py
+++ appointments/app_router.py
@@ -1,45 +1,103 @@
-from fastapi import Depends, APIRouter, status
+from fastapi import Depends, APIRouter, status, HTTPException
 from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordBearer
 from typing import Annotated
 from database_config.db_config import get_db
 from appointments.app_class import insert_appointment_class, edit_appointment_class, edit_appointment_status_class
 from appointments.app_services import get_all_appointments,edit_appointment_status, get_appointment_by_Id, get_appointment_by_patient_id, get_todays_appointment, create_new_appointment, edit_appointment
+from user_authenication.auth_service import verify_jwt_access_token
 
 db_dependency = Annotated[Session, Depends(get_db)]
 appointment_endpoint = APIRouter(prefix="/Appointment",tags=["Appointment Endpoint"])
-
+oauth2_bear = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 # This route fetch all appointments in database
 @appointment_endpoint.get("/all/{limit:int}/{offset:int}",status_code=status.HTTP_200_OK)
-def Fetch_all_appointments(db:db_dependency,limit:int,offset:int):
-    return get_all_appointments(db=db,Limit=limit,Offset=offset)
+def Fetch_all_appointments(db:db_dependency,limit:int,offset:int,token:Annotated[str,Depends(oauth2_bear)]):
+    user_info = verify_jwt_access_token(jwt_token=token,db=db)
+
+    if user_info not in ['supervisor','admin']:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User is unauthorized to view data"
+        )
+    else:
+        return get_all_appointments(db=db,Limit=limit,Offset=offset)
 
 # This route gets all appointments for today
 @appointment_endpoint.get("/todays_appointments/{limit:int}/{offset:int}",status_code=status.HTTP_200_OK)
-def Fetch_todays_appointments(db:db_dependency,limit:int,offset:int):
-    return get_todays_appointment(db=db,Limit=limit,Offset=offset)
+def Fetch_todays_appointments(db:db_dependency,limit:int,offset:int,token:Annotated[str,Depends(oauth2_bear)]):
+    user_info = verify_jwt_access_token(jwt_token=token,db=db)
+
+    if user_info not in ['supervisor','admin']:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User is unauthorized to view data"
+        )
+    else:
+        return get_todays_appointment(db=db,Limit=limit,Offset=offset)
 
 # This route fetch appiontment by appointment_id
 @appointment_endpoint.get("/by_id/{Id:str}", status_code=status.HTTP_200_OK)
-def Fetch_appointment_by_Id(db:db_dependency,Id:str):
-    return get_appointment_by_Id(db=db,Id=Id)
+def Fetch_appointment_by_Id(db:db_dependency,Id:str,token:Annotated[str,Depends(oauth2_bear)]):
+    user_info = verify_jwt_access_token(jwt_token=token,db=db)
+
+    if user_info not in ['supervisor','admin']:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User is unauthorized to view data"
+        )
+    else:
+        return  get_appointment_by_Id(db=db,Id=Id)
 
 # This route fetch all appointment by Patient Id
 @appointment_endpoint.get("/by_patient_Id/{patientId:str}/{limit:int}/{offset:int}",status_code=status.HTTP_200_OK)
-def Fetch_appointments_by_patient_Id(db:db_dependency,patientId:str,limit:int,offset:int):
-    return get_appointment_by_patient_id(db=db,patient_id=patientId,Limit=limit,Offset=offset)
+def Fetch_appointments_by_patient_Id(db:db_dependency,patientId:str,limit:int,offset:int,token:Annotated[str,Depends(oauth2_bear)]):
+    user_info = verify_jwt_access_token(jwt_token=token,db=db)
+
+    if user_info not in ['supervisor','admin']:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User is unauthorized to view data"
+        )
+    else:
+        return  get_appointment_by_patient_id(db=db,patient_id=patientId,Limit=limit,Offset=offset)
 
 # This route create new appointment in database
 @appointment_endpoint.post("/new_appointment", status_code=status.HTTP_201_CREATED)
-def Created_new_appointment(db:db_dependency,value:insert_appointment_class):
-    return create_new_appointment(db=db,appointment=value)
+def Created_new_appointment(db:db_dependency,value:insert_appointment_class,token:Annotated[str,Depends(oauth2_bear)]):
+    user_info = verify_jwt_access_token(jwt_token=token,db=db)
+
+    if user_info not in ['supervisor','admin']:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User is unauthorized to view data"
+        )
+    else:
+        return  create_new_appointment(db=db,appointment=value)
 
 # This route edit appointment data
 @appointment_endpoint.patch("/edit_appointment",status_code=status.HTTP_200_OK)
-def Edit_appointment(db:db_dependency,value:edit_appointment_class):
-    return edit_appointment(db=db,value=value)
+def Edit_appointment(db:db_dependency,value:edit_appointment_class,token:Annotated[str,Depends(oauth2_bear)]):
+    user_info = verify_jwt_access_token(jwt_token=token,db=db)
+
+    if user_info not in ['supervisor','admin']:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User is unauthorized to view data"
+        )
+    else:
+        return  edit_appointment(db=db,value=value)
 
 # This route edit appointment status
 @appointment_endpoint.patch("/edit_status_appointment",status_code=status.HTTP_200_OK)
-def Edit_appointment_status(db:db_dependency,value:edit_appointment_status_class):
-    return edit_appointment_status(db=db,app_status=value)
+def Edit_appointment_status(db:db_dependency,value:edit_appointment_status_class,token:Annotated[str,Depends(oauth2_bear)]):
+    user_info = verify_jwt_access_token(jwt_token=token,db=db)
+
+    if user_info not in ['supervisor','admin']:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User is unauthorized to view data"
+        )
+    else:
+        return  edit_appointment_status(db=db,app_status=value)

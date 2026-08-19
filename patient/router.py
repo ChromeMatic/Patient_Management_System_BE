b2dc182531d902ac409f139c04dc2878043f695b
chromeMatic <armanibrwn@gmail.com>
2026-08-18 23:27:53 -0500

    more authorization added

--- patient/router.py
+++ patient/router.py
@@ -1,39 +1,90 @@
-from fastapi import Depends, APIRouter, status
+from fastapi import Depends, APIRouter, status, HTTPException
 from sqlalchemy.orm import Session
 from typing import Annotated
+from fastapi.security import OAuth2PasswordBearer
 from database_config.db_config import get_db
 from patient.patient_class import insert_patient,edit_patient, next_of_kin, Patient_Search
 from patient.services import get_all_patients, get_patient_by_ID, create_new_patient, edit_patient_record, get_all_next_of_kin_record, search_by
+from user_authenication.auth_service import verify_jwt_access_token
 
 patient_endpoint = APIRouter( prefix="/Patient",tags=["Patient Endpoints"])
 db_dependency = Annotated[Session, Depends(get_db)]
+oauth2_bear = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 # This endpoint gets all patients information
 @patient_endpoint.get("/all/{limit:int}/{offset:int}",status_code=status.HTTP_200_OK)
-def Fetch_all_patients(db:db_dependency,limit:int,offset:int):
-    return get_all_patients(db=db,limnit=limit,offset=offset)
+def Fetch_all_patients(db:db_dependency,limit:int,offset:int,token:Annotated[str,Depends(oauth2_bear)]):
+    user_info = verify_jwt_access_token(jwt_token=token,db=db)
+
+    if user_info not in ['supervisor','admin']:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User is unauthorized to view data"
+        )
+    else:
+        return get_all_patients(db=db,limnit=limit,offset=offset)
 
 # This endpoint gets patient by ID
 @patient_endpoint.get("/by-Id/{ID:str}",status_code=status.HTTP_200_OK)
-def Fetch_patient_by_id(db:db_dependency,ID:str):
-    return get_patient_by_ID(db=db,Id=ID)
+def Fetch_patient_by_id(db:db_dependency,ID:str,token:Annotated[str,Depends(oauth2_bear)]):
+    user_info = verify_jwt_access_token(jwt_token=token,db=db)
+
+    if user_info not in ['supervisor','admin']:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User is unauthorized to view data"
+        )
+    else:
+        return get_patient_by_ID(db=db,Id=ID)
 
 # This endpoint fetch all next of kin record
 @patient_endpoint.get("/all-next-of-kin/{limit:int}/{offset:int}")
-def Fetch_all_next_of_kin_record(db:db_dependency,limit:int,offset:int):
-    return get_all_next_of_kin_record(db=db,limnit=limit,offset=offset)
+def Fetch_all_next_of_kin_record(db:db_dependency,limit:int,offset:int,token:Annotated[str,Depends(oauth2_bear)]):
+    user_info = verify_jwt_access_token(jwt_token=token,db=db)
+
+    if user_info not in ['supervisor','admin']:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User is unauthorized to view data"
+        )
+    else:
+        return get_all_next_of_kin_record(db=db,limnit=limit,offset=offset)
 
 # This endpoint handles patient search
 @patient_endpoint.post("/search", status_code=status.HTTP_200_OK)
-def Search_Patient(db:db_dependency,Value:Patient_Search):
-    return search_by(db=db,value=Value)
+def Search_Patient(db:db_dependency,Value:Patient_Search,token:Annotated[str,Depends(oauth2_bear)]):
+    user_info = verify_jwt_access_token(jwt_token=token,db=db)
+
+    if user_info not in ['supervisor','admin']:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User is unauthorized to view data"
+        )
+    else:
+        return search_by(db=db,value=Value)
 
 # This endpoint create new patient record
 @patient_endpoint.post("/create-new-patient",status_code=status.HTTP_201_CREATED)
-def Create_new_patient(db:db_dependency,patient:insert_patient,kin:next_of_kin):
-    return create_new_patient(db=db,new_record=patient,new_kin=kin)
+def Create_new_patient(db:db_dependency,patient:insert_patient,kin:next_of_kin,token:Annotated[str,Depends(oauth2_bear)]):
+    user_info = verify_jwt_access_token(jwt_token=token,db=db)
+
+    if user_info not in ['supervisor','admin']:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User is unauthorized to view data"
+        )
+    else:
+        return create_new_patient(db=db,new_record=patient,new_kin=kin)
 
 # This endpoint edit patient record
 @patient_endpoint.patch("/edit-patient",status_code=status.HTTP_200_OK)
-def Edit_patient_record(db:db_dependency,patient:edit_patient):
-    return edit_patient_record(db=db,record=patient)
+def Edit_patient_record(db:db_dependency,patient:edit_patient,token:Annotated[str,Depends(oauth2_bear)]):
+    user_info = verify_jwt_access_token(jwt_token=token,db=db)
+
+    if user_info not in ['supervisor','admin']:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User is unauthorized to view data"
+        )
+    else:
+        return edit_patient_record(db=db,record=patient)
--- patient_notes/rec_router.py
+++ patient_notes/rec_router.py
@@ -1,29 +1,64 @@
-from fastapi import Depends, APIRouter, status
+from fastapi import Depends, APIRouter, status, HTTPException
 from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordBearer
 from typing import Annotated
 from database_config.db_config import get_db
 from patient_notes.rec_class import patient_note_insert, patient_note_edit
 from patient_notes.rec_services import get_patient_note_by_Id, get_patient_notes_by_patientID, create_new_notes_record, edit_patient_note_record
+from user_authenication.auth_service import verify_jwt_access_token
 
 notes_router =  APIRouter( prefix="/Notes",tags=["Patient Notes Endpoint"])
 db_dependency = Annotated[Session, Depends(get_db)]
+oauth2_bear = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 # Fetch notes by note_ID
 @notes_router.get("/by_id/{Id:str}",status_code=status.HTTP_200_OK)
-def Fetch_by_notes_Id(db:db_dependency,Id:str):
-    return get_patient_note_by_Id(db=db,Id=Id)
+def Fetch_by_notes_Id(db:db_dependency,Id:str,token:Annotated[str,Depends(oauth2_bear)]):
+    user_info = verify_jwt_access_token(jwt_token=token,db=db)
+
+    if user_info not in ['supervisor','admin']:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User is unauthorized to view data"
+        )
+    else:
+        return get_patient_note_by_Id(db=db,Id=Id)
 
 # Fetch notes by patient ID
 @notes_router.get("/notes_by_patient_id/{patientId}/{Limit}/{Offset}",status_code=status.HTTP_200_OK)
-def Fetch_notes_by_patient_id(db:db_dependency,Limit:int,Offset:int,patient_id:str):
-    return get_patient_notes_by_patientID(db=db,patient_id=patient_id,limit=Limit,offset=Offset)
+def Fetch_notes_by_patient_id(db:db_dependency,Limit:int,Offset:int,patient_id:str,token:Annotated[str,Depends(oauth2_bear)]):
+    user_info = verify_jwt_access_token(jwt_token=token,db=db)
+
+    if user_info not in ['supervisor','admin']:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User is unauthorized to view data"
+        )
+    else:
+        return get_patient_notes_by_patientID(db=db,patient_id=patient_id,limit=Limit,offset=Offset)
 
 # Create new notes endpoint
 @notes_router.post("/create",status_code=status.HTTP_201_CREATED)
-def Create_new_notes_endpoint(db:db_dependency,new_record:patient_note_insert):
-    return create_new_notes_record(db=db,record=new_record)
+def Create_new_notes_endpoint(db:db_dependency,new_record:patient_note_insert,token:Annotated[str,Depends(oauth2_bear)]):
+    user_info = verify_jwt_access_token(jwt_token=token,db=db)
+
+    if user_info not in ['supervisor','admin']:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User is unauthorized to view data"
+        )
+    else:
+        return create_new_notes_record(db=db,record=new_record)
 
 # Edit notes endpoint
 @notes_router.patch("/edit",status_code=status.HTTP_200_OK)
-def Edit_notes_endpoint(db:db_dependency,record:patient_note_edit):
-    return edit_patient_note_record(db=db,record=record)
+def Edit_notes_endpoint(db:db_dependency,record:patient_note_edit,token:Annotated[str,Depends(oauth2_bear)]):
+    user_info = verify_jwt_access_token(jwt_token=token,db=db)
+
+    if user_info not in ['supervisor','admin']:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User is unauthorized to view data"
+        )
+    else:
+        return edit_patient_note_record(db=db,record=record)
